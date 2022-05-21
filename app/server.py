73211e21d6671b9606742c3a6d7c3b0f98aec342
tvaingart <tvaingart@salesforce.com>
2022-05-21 15:33:15 +0300

    servig model with server

--- app/server.py
+++ app/server.py
@@ -11,7 +11,7 @@ import uvicorn, aiohttp, asyncio
 import sys, numpy as np
 
 path = Path(__file__).parent
-model_file_url = 'YOUR MODEL.h5 DIRECT / RAW DOWNLOAD URL HERE!'
+model_file_url = 'hhttps://github.com/tvaingart/bone-age-assessment-webapp/blob/main/models/female_model_weights_resnet.h5?raw=true'
 model_file_name = 'model'
 
 app = Starlette()
@@ -20,6 +20,46 @@ app.mount('/static', StaticFiles(directory='app/static'))
 
 MODEL_PATH = path/'models'/f'{model_file_name}.h5'
 IMG_FILE_SRC = '/tmp/saved_image.png'
+
+# get resnet model
+def get_resnet_model(input_shape=(384, 384, 3)):
+  in_lay = Input(input_shape)
+
+  base_pretrained_model = ResNet50(input_shape =  input_shape, include_top = False, weights = 'imagenet')
+
+  #base_pretrained_model.summary()
+  base_pretrained_model.trainable = False
+  pt_features = base_pretrained_model(in_lay)
+  pt_depth = base_pretrained_model.get_output_shape_at(0)[-1]
+  bn_features = BatchNormalization()(pt_features)
+
+  # here we do an attention mechanism to turn pixels in the GAP on an off
+
+  attn_layer = Conv2D(64, kernel_size = (1,1), padding = 'same', activation = 'relu')(bn_features)
+  attn_layer = Conv2D(16, kernel_size = (1,1), padding = 'same', activation = 'relu')(attn_layer)
+  attn_layer = LocallyConnected2D(1, 
+                                  kernel_size = (1,1), 
+                                  padding = 'valid', 
+                                  activation = 'sigmoid')(attn_layer)
+  # fan it out to all of the channels
+  up_c2_w = np.ones((1, 1, 1, pt_depth))
+  up_c2 = Conv2D(pt_depth, kernel_size = (1,1), padding = 'same', 
+                activation = 'linear', use_bias = False, weights = [up_c2_w])
+  up_c2.trainable = False
+  attn_layer = up_c2(attn_layer)
+
+  mask_features = multiply([attn_layer, bn_features])
+  gap_features = GlobalAveragePooling2D()(mask_features)
+  gap_mask = GlobalAveragePooling2D()(attn_layer)
+  # to account for missing values from the attention model
+  gap = Lambda(lambda x: x[0]/x[1], name = 'RescaleGAP')([gap_features, gap_mask])
+  gap_dr = Dropout(0.5)(gap)
+  dr_steps = Dropout(0.25)(Dense(1024, activation = 'elu')(gap_dr))
+  out_layer = Dense(1, activation = 'linear')(dr_steps) # linear is what 16bit did
+  bone_age_model = Model(inputs = [in_lay], outputs = [out_layer])
+  #bone_age_model.summary()
+  return bone_age_model
+
 
 async def download_file(url, dest):
     if dest.exists(): return
@@ -33,7 +73,8 @@ async def setup_model():
     # await download_file(model_file_url, MODEL_PATH)
     # model = load_model(MODEL_PATH) # Load your Custom trained model
     # model._make_predict_function()
-    model = ResNet50(weights='imagenet') # COMMENT, IF you have Custom trained model
+    model = get_resnet_model()
+    #model = ResNet50(weights='imagenet') # COMMENT, IF you have Custom trained model
     return model
 
 # Asynchronous Steps
